64efd4a84aec082cf1bf22e1a7949b254b985cf7
lennonbe <d.lennonbeato@lancaster.ac.uk>
2020-10-25 18:28:00 +0000

    Added the changing of icons in all squares except for the ones which have 0 bombs around them. These still need to be coded including the recursive algorithm

--- BombSquare.java
+++ BombSquare.java
@@ -1,8 +1,9 @@
 public class BombSquare extends GameSquare
 {
     private GameBoard board;                            // Object reference to the GameBoard this square is part of.
-    private boolean hasBomb, rightClickFlag;                            // True if this squre contains a bomb. False otherwise.
-    private int count;
+    private boolean hasBomb, rightClickFlag, beenSet;                            // True if this squre contains a bomb. False otherwise.
+    private int count = 0;
+    private BombSquare temp;
 
 	public static final int MINE_PROBABILITY = 10;
 
@@ -12,7 +13,44 @@ public class BombSquare extends GameSquare
 
         this.board = board;
         this.hasBomb = (((int) (Math.random() * MINE_PROBABILITY)) == 0);
-    }  
+        this.beenSet = false;
+
+        //this.setCount();
+    }
+
+    public boolean hasBomb()
+    {
+        return hasBomb;
+    }
+    
+    public void setCount()
+    {
+        if(!beenSet)
+        {
+            beenSet = true;
+            for(int i = -1; i < 2; i++)
+            {
+                for(int j = -1; j < 2; j++)
+                {
+                    if(this.getXLocation() + i < 0 || this.getXLocation() + i > 29 || this.getYLocation() + j < 0 || this.getYLocation() + j > 29)
+                    {
+                        //do nothing, as this square does not exist
+                    }
+                    else
+                    {
+                        temp = (BombSquare) board.getSquareAt(this.getXLocation() + i, this.getYLocation() + j);
+
+                        if(temp.hasBomb())
+                        {
+                            this.count++;
+                        }
+
+                        System.out.println("Square" + this.getXLocation() + " " + this.getYLocation() + "has count = " + this.count);
+                    }
+                }
+            }
+        }
+    }
     
     public void leftClicked()
     {
@@ -21,6 +59,8 @@ public class BombSquare extends GameSquare
         All squares should have a value regarding the no. of mines adjacent to them.
         */
 
+        this.setCount();
+
         if(this.hasBomb)
         {
             this.setImage("images/bomb.png");
@@ -28,6 +68,10 @@ public class BombSquare extends GameSquare
         else
         {
             this.clearSquare();
+            /*if(this.count == 0)
+            {
+
+            }*/
         }
     }
 
@@ -47,6 +91,61 @@ public class BombSquare extends GameSquare
 
     public void clearSquare()
     {
-        board.getSquareAt(this.getXLocation() - 1, this.getYLocation()).setImage("images/0.png");
+        if(this.count == 0)
+        {
+            /*this.setImage("images/0.png");
+            for(int i = -1; i < 2; i++)
+            {
+                for(int j = -1; j < 2; j++)
+                {
+                    if(this.getXLocation() + i < 0 || this.getXLocation() + i > 29 || this.getYLocation() + j < 0 || this.getYLocation() + j > 29)
+                    {
+                        //do nothing, as this square does not exist
+                    }
+                    else
+                    {
+                        temp = (BombSquare) board.getSquareAt(this.getXLocation() + i, this.getYLocation() + j);
+                        temp.clearSquare();
+
+                    }
+                }
+            }*/
+        }
+        else
+        {
+            if(this.count == 1)
+            {
+                this.setImage("images/1.png");
+            }
+            else if(this.count == 2)
+            {
+                this.setImage("images/2.png");
+            }
+            else if(this.count == 3)
+            {
+                this.setImage("images/3.png");
+            }
+            else if(this.count == 4)
+            {
+                this.setImage("images/4.png");
+            }
+            else if(this.count == 5)
+            {
+                this.setImage("images/5.png");
+            }
+            else if(this.count == 6)
+            {
+                this.setImage("images/6.png");
+            }
+            else if(this.count == 7)
+            {
+                this.setImage("images/7.png");
+            }
+            else if(this.count == 8)
+            {
+                this.setImage("images/8.png");
+            }
+        }
     }
+    
 }
