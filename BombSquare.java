5cacdb432dd01f1cd6151dff628e2bc16b190ba3
lennonbe <d.lennonbeato@lancaster.ac.uk>
2020-10-27 20:36:35 +0000

    Recursive method finally works, stackoverflow errors have been foxed.

--- BombSquare.java
+++ BombSquare.java
@@ -4,9 +4,9 @@ public class BombSquare extends GameSquare
     private boolean hasBomb, rightClickFlag, beenSet, beenCleared;                            // True if this squre contains a bomb. False otherwise.
     private int count = 0;
     private int arrSize = 0;
-    private BombSquare temp;
-    private BombSquare [] surroundingSquares;
-    private int surrCount = 0;
+    private BombSquare temp, surroundingBombsquare;
+    //private BombSquare [] surroundingSquares;
+    //private int surrCount = 0;
 
 	public static final int MINE_PROBABILITY = 10;
 
@@ -23,35 +23,25 @@ public class BombSquare extends GameSquare
 
         //this.setCount();
     }
-
-    public boolean hasBomb()
-    {
-        return hasBomb;
-    }
     
     public void setCount()
     {
-        if(!this.beenSet)
+        
+        for(int i = -1; i < 2; i++)
         {
-            this.beenSet = true;
-            for(int i = -1; i < 2; i++)
+            for(int j = -1; j < 2; j++)
             {
-                for(int j = -1; j < 2; j++)
+                if(this.getXLocation() + i < 0 || this.getXLocation() + i > 29 || this.getYLocation() + j < 0 || this.getYLocation() + j > 29)
                 {
-                    if(this.getXLocation() + i < 0 || this.getXLocation() + i > 29 || this.getYLocation() + j < 0 || this.getYLocation() + j > 29)
+                    //do nothing, as this square does not exist
+                }
+                else
+                {
+                    
+                    temp = (BombSquare) board.getSquareAt(this.getXLocation() + i, this.getYLocation() + j);
+                    if(temp.hasBomb)
                     {
-                        //do nothing, as this square does not exist
-                    }
-                    else
-                    {
-                        temp = (BombSquare) board.getSquareAt(this.getXLocation() + i, this.getYLocation() + j);
-
-                        if(temp.hasBomb())
-                        {
-                            this.count++;
-                        }
-
-                        //System.out.println("Square" + this.getXLocation() + " " + this.getYLocation() + "has count = " + this.count);
+                        this.count++;
                     }
                 }
             }
@@ -92,84 +82,74 @@ public class BombSquare extends GameSquare
     public void clearSquare()
     {
         this.setCount();
-        //this.surrCount = 0;
 
-        if(!this.beenCleared)
+        for(int i = -1; i < 2; i++)
         {
-            if(this.count == 0 /*&& !this.beenCleared*/)
+            for(int j = -1; j < 2; j++)
             {
-                this.setImage("images/0.png");
-
-                /*this.surroundingSquares = new BombSquare[this.getNumSurroundingElements()];
-                this.populateSurroundingElementsArr();
-
-                for(int i = 0; i < this.surroundingSquares.length; i++)
+                if(!(i == 0 && j == 0))
                 {
-                    this.surroundingSquares[i].clearSquare();
-                }*/
-
-                for(int i = -1; i < 2; i++)
-                {
-                    for(int j = -1; j < 2; j++)
+                    if(this.getXLocation() + i < 0 || this.getXLocation() + i > 29 || this.getYLocation() + j < 0 || this.getYLocation() + j > 29)
                     {
-                        if(!(i == 0 && j == 0))
+                        //do nothing, as this square does not exist
+                    }
+                    else
+                    {
+                        surroundingBombsquare = (BombSquare) this.board.getSquareAt(this.getXLocation() + i, this.getYLocation() + j);
+                        if(!surroundingBombsquare.beenCleared && !this.surroundingBombsquare.hasBomb)
                         {
-                            if(this.getXLocation() + i < 0 || this.getXLocation() + i > 29 || this.getYLocation() + j < 0 || this.getYLocation() + j > 29)
+                            this.beenCleared = true;
+                            
+                            if(this.count == 0)
                             {
-                                //do nothing, as this square does not exist
+                                this.setImage("images/0.png");
+                                surroundingBombsquare.clearSquare();
                             }
                             else
                             {
-                                ((BombSquare) this.board.getSquareAt(this.getXLocation() + i, this.getYLocation() + j)).clearSquare();
+                                if(this.count == 1)
+                                {
+                                    this.setImage("images/1.png");
+                                }
+                                else if(this.count == 2)
+                                {
+                                    this.setImage("images/2.png");
+                                }
+                                else if(this.count == 3)
+                                {
+                                    this.setImage("images/3.png");
+                                }
+                                else if(this.count == 4)
+                                {
+                                    this.setImage("images/4.png");
+                                }
+                                else if(this.count == 5)
+                                {
+                                    this.setImage("images/5.png");
+                                }
+                                else if(this.count == 6)
+                                {
+                                    this.setImage("images/6.png");
+                                }
+                                else if(this.count == 7)
+                                {
+                                    this.setImage("images/7.png");
+                                }
+                                else if(this.count == 8)
+                                {
+                                    this.setImage("images/8.png");
+                                }
                             }
                         }
-                        
                     }
                 }
                 
             }
-            else //if(this.count != 0 && !this.beenCleared)
-            {
-                if(this.count == 1)
-                {
-                    this.setImage("images/1.png");
-                }
-                else if(this.count == 2)
-                {
-                    this.setImage("images/2.png");
-                }
-                else if(this.count == 3)
-                {
-                    this.setImage("images/3.png");
-                }
-                else if(this.count == 4)
-                {
-                    this.setImage("images/4.png");
-                }
-                else if(this.count == 5)
-                {
-                    this.setImage("images/5.png");
-                }
-                else if(this.count == 6)
-                {
-                    this.setImage("images/6.png");
-                }
-                else if(this.count == 7)
-                {
-                    this.setImage("images/7.png");
-                }
-                else if(this.count == 8)
-                {
-                    this.setImage("images/8.png");
-                }
-            }
-
-            this.beenCleared = true;
+        
         }
     }
-    
 
-    public int getNumSurroundingElements()
+    /*public int getNumSurroundingElements()
     {
         arrSize = 0;
 
@@ -219,5 +199,5 @@ public class BombSquare extends GameSquare
                 
             }
         }
-    }
+    }*/
 }
