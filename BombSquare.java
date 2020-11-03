93a05988927853ab639617f5839558e3129da9df
lennonbe <d.lennonbeato@lancaster.ac.uk>
2020-11-03 15:00:57 +0000

    Working code, just submitting for safety reasons

--- BombSquare.java
+++ BombSquare.java
@@ -2,10 +2,10 @@ public class BombSquare extends GameSquare
 {
     private GameBoard board;                            // Object reference to the GameBoard this square is part of.
     private boolean hasBomb, rightClickFlag, beenCleared;                            // True if this squre contains a bomb. False otherwise.
-    private static boolean gameOver;
     private int count = 0;
     private BombSquare temp;
 
+    private static boolean gameOver;
 	public static final int MINE_PROBABILITY = 10;
 
 	public BombSquare(int x, int y, GameBoard board)
@@ -57,7 +57,6 @@ public class BombSquare extends GameSquare
                 }
                 else
                 {
-                    
                     temp = (BombSquare) board.getSquareAt(this.getXLocation() + i, this.getYLocation() + j);
                     if(temp.hasBomb)
                     {
@@ -76,7 +75,6 @@ public class BombSquare extends GameSquare
         {
             for(int j = -1; j < 2; j++)
             {
-                
                 if(this.getXLocation() + i < 0 || this.getXLocation() + i > 29 || this.getYLocation() + j < 0 || this.getYLocation() + j > 29)
                 {
                     //do nothing, as this square does not exist
@@ -95,13 +93,9 @@ public class BombSquare extends GameSquare
                         }
 
                         this.setImage("images/" + String.valueOf(this.count) + ".png");
-                        
                     }
                 }
-                
             }
-        
         }
     }
-
 }
